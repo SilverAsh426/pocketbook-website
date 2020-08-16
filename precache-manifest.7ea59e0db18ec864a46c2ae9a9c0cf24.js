e54dc03e36c4cf2ed1758e89ea0b0397dbe8e5c1
SilverAsh <qingfeng26@foxmail.com>
2020-08-16 15:13:36 +0800

    deploy

---
+++ precache-manifest.7ea59e0db18ec864a46c2ae9a9c0cf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c6a4d7e4e908128045",
+    "url": "/pocketbook-website/css/app.c4fed8e0.css"
+  },
+  {
+    "revision": "dde5fd12beaf292f76ac3a71c4820c63",
+    "url": "/pocketbook-website/index.html"
+  },
+  {
+    "revision": "e9c6a4d7e4e908128045",
+    "url": "/pocketbook-website/js/app.d1dba30a.js"
+  },
+  {
+    "revision": "d598448783068317f197",
+    "url": "/pocketbook-website/js/chunk-vendors.aec3d1c8.js"
+  },
+  {
+    "revision": "c7cafa20509f4b6e5a11573fcd3c974e",
+    "url": "/pocketbook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pocketbook-website/robots.txt"
+  }
+]);
